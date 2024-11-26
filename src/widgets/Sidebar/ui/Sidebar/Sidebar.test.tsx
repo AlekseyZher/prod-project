22110716cc7a0d3f3b7b28422e0948487a991ab3
Aleksey <alekszher@mail.ru>
2024-11-26 20:02:28 +0600

    fix test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,16 @@
 import { fireEvent, screen } from '@testing-library/react';
+import { componentRender } from 'shared/config/lib/tests/componentsRender/componentsRender';
 import { renderWidthTranslations }
 	from 'shared/config/lib/tests/renderWidthTranslations/renderWidthTranslations';
 import { Sidebar } from 'widgets/Sidebar';
 
 describe('Sidebar', () => {
 	test('test render', () => {
-		renderWidthTranslations(<Sidebar />);
+		componentRender(<Sidebar />);
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument();
 	});
 	test('test sidebar toggle', () => {
-		renderWidthTranslations(<Sidebar />);
+		componentRender(<Sidebar />);
 		const toggleBtn = screen.getByTestId('sidebar-toggle');
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument();
 		fireEvent.click(toggleBtn);
